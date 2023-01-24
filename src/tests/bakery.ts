acb4f69e1da43a34872c008506c0e5d1a86c4bd8
yujin <kimyu28jin@gmail.com>
2023-01-24 22:02:05 +0900

    test: table 컴포넌트 테스트코드 작성

--- src/tests/bakery.ts
+++ src/tests/bakery.ts
@@ -1,21 +1,21 @@
 export const fakeBakeries = [
   {
     bakeryId: 3,
-    name: '테스트 신규_성수점 2',
+    name: '테스트 성수점',
     createdAt: '2023-01-15',
     modifiedAt: '2023-01-16',
     status: 'UNPOSTING',
   },
   {
     bakeryId: 2,
-    name: '테스트 신규2',
+    name: '빵빵 강남점',
     createdAt: '2023-01-15',
     modifiedAt: '2023-01-15',
     status: 'POSTING',
   },
   {
     bakeryId: 1,
-    name: '테스트 신규1',
+    name: '일산 브레드',
     createdAt: '2023-01-15',
     modifiedAt: '2023-01-15',
     status: 'UNPOSTING',
