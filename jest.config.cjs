c3aabea65baf400c787f9dc871ce062f0da0f7e2
yujin <kimyu28jin@gmail.com>
2023-01-24 21:58:06 +0900

    chore: test 환경 수정, test lib import 적용

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/file.ts",
     "\\.(css|less)$": "<rootDir>/src/tests/file.ts"
   },
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
 };
