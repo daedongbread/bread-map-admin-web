f4ae2234bc84ae4d369b3ced4207de291084fa54
yujin <kimyu28jin@gmail.com>
2023-02-18 22:54:57 +0900

    feat: storybook input 추가

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,59 @@
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { Input } from '@/components/Shared';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'Daedong/Input',
+  component: Input,
+  argTypes: {
+    type: {
+      control: false,
+    },
+    name: {
+      control: false,
+    },
+    textType: {
+      options: ['text', 'number'],
+      control: { type: 'radio' },
+    },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = args => <Input {...args} />;
+
+export const Plain = Template.bind({});
+Plain.args = {
+  type: 'plain',
+  textarea: false,
+  textType: 'text',
+  padding: 'small',
+  placeholder: '텍스트를 입력해주세요.',
+};
+
+export const Gray = Template.bind({});
+Gray.args = {
+  type: 'gray',
+  textarea: false,
+  textType: 'text',
+  padding: 'small',
+  placeholder: '텍스트를 입력해주세요.',
+};
+
+export const Orange = Template.bind({});
+Orange.args = {
+  type: 'orange',
+  textarea: false,
+  textType: 'text',
+  padding: 'small',
+  placeholder: '텍스트를 입력해주세요.',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  type: 'disabled',
+  textarea: false,
+  textType: 'text',
+  padding: 'small',
+  disabled: true,
+  placeholder: '텍스트를 입력할 수 없는 인풋입니다.',
+};
