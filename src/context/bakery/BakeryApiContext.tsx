0597d2476c7aeb7eff7f0a55adf0e9b6365cedbd
yujin <kimyu28jin@gmail.com>
2023-02-23 23:37:22 +0900

    fix: mutation 관련에러로 인해 임시로 수정

--- src/context/bakery/BakeryApiContext.tsx
+++ src/context/bakery/BakeryApiContext.tsx
@@ -4,5 +4,8 @@ import { Bakery } from '@/apis/bakery/bakery';
 export const BakeryApiContext = createContext<{ bakery: Bakery | null }>({ bakery: null });
 
 export const useBakeryApi = () => {
-  return useContext(BakeryApiContext);
+  const context = useContext(BakeryApiContext);
+  if (!context) throw new Error('bakery context가 없습니다.');
+
+  return context;
 };
