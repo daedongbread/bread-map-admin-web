e90c973ff19117f467af735f8e4a0cd7777f10fb
yujin <kimyu28jin@gmail.com>
2023-03-07 14:56:51 +0900

    refactor: 기존 빵집상세 구조 리팩토링

--- src/components/BakeryDetail/Form/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AddressArea';
-export * from './BakeryImgField';
-export * from './MenuArea';
-export * from './SnsLinkArea';
-export * from './TextField';
--- src/containers/BakeryDetail/BakeryDetailContainer.tsx
+++ src/containers/BakeryDetail/BakeryDetailContainer.tsx
@@ -1,36 +1,18 @@
-import React, { ChangeEvent, useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { BakerySns } from '@/apis';
 import { useBakery } from '@/apis/bakery/useBakery';
-import { AddressArea, BakeryImgField, MenuArea, SnsLink, SnsLinkArea, TextField } from '@/components/BakeryDetail/Form';
 import { ReportTab } from '@/components/BakeryDetail/Report';
 import { Button, SelectBox, StatusSelectTrigger, StatusSelectOption, SelectOption } from '@/components/Shared';
 import { BAKERY_REPORT_TAB, BAKERY_STATUS_OPTIONS } from '@/constants';
 import useSelectBox from '@/hooks/useSelectBox';
 import useTab from '@/hooks/useTab';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import {
-  initializeForm,
-  setForm,
-  setLinks,
-  changeForm,
-  changeBakeryStatus,
-  BakeryFormChangeKey,
-  changeBakeryImg,
-  toggleLinkOption,
-  selectLinkOption,
-  changeLinkValue,
-  removeLink,
-  addLink,
-  changeMenuInput,
-  removeMenu,
-  addMenu,
-  changeMenuImg,
-  toggleMenuTypeOption,
-  selectMenuTypeOption,
-} from '@/store/slices/bakery';
+import { initializeForm, setForm, setLinks, changeBakeryStatus, changeBakeryImg } from '@/store/slices/bakery';
 import { makeBakeryPayload } from '@/utils';
 import styled from '@emotion/styled';
+import { SnsLink } from '@/components/BakeryDetail/Form/SnsLinkArea';
+import { BakeryForm } from '@/components/BakeryDetail/Form';
 
 export const BakeryDetailContainer = () => {
   const { bakeryId } = useParams();
@@ -45,7 +27,6 @@ export const BakeryDetailContainer = () => {
 
   // opened state를 리덕스에 저장하면 안될거같은데..?
   const { form, formLinks, openedSnsLinkIdx, openedMenuTypeIdx } = useAppSelector(selector => selector.bakery);
-  const { name, image, address, latitude, longitude, hours, phoneNumber, productList } = form;
 
   const { isOpen, selectedOption, onToggleSelectBox, onSelectOption } = useSelectBox(BAKERY_STATUS_OPTIONS[0]);
   const { tabs: reportTabs, selectTab: selectReportTab } = useTab({ tabData: BAKERY_REPORT_TAB });
@@ -83,73 +64,12 @@ export const BakeryDetailContainer = () => {
     bakeryId ? onUpdateForm(payload) : onCreateForm(payload);
   };
 
-  const onChangeForm = useCallback((payload: { name: BakeryFormChangeKey; value: never }) => {
-    dispatch(changeForm(payload));
-  }, []);
-
   const onSelectBakerysSatusOption = (status: SelectOption | null) => {
     if (!status) {
       return;
     }
     onSelectOption(status);
     dispatch(changeBakeryStatus({ status: status.value }));
-  };
-
-  const onChangeBakeryImg = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    const imgPreview = URL.createObjectURL(e.target.files[0]);
-    dispatch(changeBakeryImg({ imgPreview }));
-  };
-
-  const onToggleLinkOption = useCallback((currIdx: number) => {
-    dispatch(toggleLinkOption({ currIdx }));
-  }, []);
-
-  const onSelectLinkOption = useCallback((payload: { currIdx: number; optionValue: string; linkValue: string }) => {
-    dispatch(selectLinkOption(payload));
-  }, []);
-
-  const onChangeLinkValue = useCallback((payload: { currIdx: number; optionValue: string; linkValue: string }) => {
-    dispatch(changeLinkValue(payload));
-  }, []);
-
-  const onSetLinks = useCallback((links: SnsLink[]) => {
-    dispatch(setLinks({ links }));
-  }, []);
-
-  const onRemoveLink = useCallback((currIdx: number) => {
-    dispatch(removeLink({ currIdx }));
-  }, []);
-
-  const onAddLink = useCallback(() => {
-    dispatch(addLink());
-  }, []);
-
-  const onToggleMenuTypeOption = (currIdx: number) => {
-    dispatch(toggleMenuTypeOption({ currIdx }));
-  };
-
-  const onSelectMenuTypeOption = ({ currIdx, optionValue }: { currIdx: number; optionValue: string }) => {
-    dispatch(selectMenuTypeOption({ currIdx, optionValue }));
-  };
-
-  const onChangeMenuInput = (payload: { currIdx: number; name: string; value: string }) => {
-    dispatch(changeMenuInput(payload));
-  };
-
-  const onRemoveMenu = (currIdx: number) => {
-    dispatch(removeMenu({ currIdx }));
-  };
-
-  const onAddMenu = () => {
-    dispatch(addMenu());
-  };
-
-  const onChangeMenuImg = ({ currIdx, e }: { currIdx: number; e: ChangeEvent<HTMLInputElement> }) => {
-    if (!e.target.files) return;
-
-    const imgPreview = URL.createObjectURL(e.target.files[0]);
-    dispatch(changeMenuImg({ currIdx, imgPreview }));
   };
 
   const onCreateForm = (payload: FormData) => {
@@ -190,42 +110,7 @@ export const BakeryDetailContainer = () => {
       </Header>
       <ScrollViewContainer>
         <ScrollSection>
-          <Forms>
-            <TextField label={'빵집명'} name={'name'} value={name} onChangeForm={onChangeForm} />
-            <BakeryImgField label={'대표이미지'} previewImg={image} onChangeBakeryImg={onChangeBakeryImg} />
-            <AddressArea label={'주소'} fullAddress={{ address, latitude, longitude }} onChangeForm={onChangeForm} />
-            <TextField
-              textarea
-              label={'시간'}
-              name={'hours'}
-              value={hours || ''}
-              onChangeForm={onChangeForm}
-              placeholder={'엔터키를 치면 줄바꿈이 적용됩니다.'}
-            />
-            <SnsLinkArea
-              label={'홈페이지'}
-              snsLinks={formLinks}
-              openedLinkIdx={openedSnsLinkIdx}
-              onToggleLinkOption={onToggleLinkOption}
-              onSelectLinkOption={onSelectLinkOption}
-              onChangeLinkValue={onChangeLinkValue}
-              onSetLinks={onSetLinks}
-              onRemoveLink={onRemoveLink}
-              onAddLink={onAddLink}
-            />
-            <TextField label={'전화번호'} name={'phoneNumber'} value={phoneNumber || ''} onChangeForm={onChangeForm} placeholder={'000-000-0000'} />
-            <MenuArea
-              label={'메뉴'}
-              menus={productList}
-              openedMenuTypeIdx={openedMenuTypeIdx}
-              onToggleMenuTypeOption={onToggleMenuTypeOption}
-              onSelectMenuTypeOption={onSelectMenuTypeOption}
-              onChangeMenuInput={onChangeMenuInput}
-              onRemoveMenu={onRemoveMenu}
-              onAddMenu={onAddMenu}
-              onChangeMenuImg={onChangeMenuImg}
-            />
-          </Forms>
+          <BakeryForm />
         </ScrollSection>
         <ScrollSection>
           <div>
@@ -240,11 +125,6 @@ export const BakeryDetailContainer = () => {
     </Container>
   );
 };
-
-const Forms = styled.form`
-  padding-top: 2rem;
-  margin-bottom: 10rem;
-`;
 
 const SaveBtns = styled.div`
   display: flex;
