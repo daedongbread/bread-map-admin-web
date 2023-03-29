d8837bc71962e56eb5704c9d38196237ebd6fb4e
yjkim <kimyu28jin@gmail.com>
2023-03-29 16:56:56 +0900

    feat: 변경된 api 반영

--- src/apis/bakery/bakeryClient.ts
+++ src/apis/bakery/bakeryClient.ts
@@ -3,12 +3,12 @@ import { fetcher } from '@/apis/axios';
 
 export class BakeryClient implements BakeryApiClient {
   async getItem({ bakeryId }: { bakeryId: number }) {
-    const resp = await fetcher.get<BakeryDetailEntity>(`bakery/${bakeryId}`);
+    const resp = await fetcher.get<BakeryDetailEntity>(`bakeries/${bakeryId}`);
     return resp.data;
   }
 
   async createItem({ payload }: CreateUpdateBakeryPayload) {
-    await fetcher.post('bakery', payload, {
+    await fetcher.post('bakeries', payload, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -16,7 +16,7 @@ export class BakeryClient implements BakeryApiClient {
   }
 
   async updateItem({ bakeryId, payload }: { bakeryId: number } & CreateUpdateBakeryPayload) {
-    await fetcher.post(`bakery/${bakeryId}`, payload, {
+    await fetcher.post(`bakeries/${bakeryId}`, payload, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -24,12 +24,12 @@ export class BakeryClient implements BakeryApiClient {
   }
 
   async getList({ page }: Omit<GetBakeriesPayload, 'name'>) {
-    const resp = await fetcher.get<GetBakeriesResponse>(`/bakery`, { params: { page } });
+    const resp = await fetcher.get<GetBakeriesResponse>(`/bakeries`, { params: { page } });
     return { bakeries: resp.data.contents, totalCount: resp.data.totalElements, totalPages: resp.data.totalPages };
   }
 
   async searchList({ name, page }: GetBakeriesPayload) {
-    const resp = await fetcher.get<GetBakeriesResponse>('/bakery/search', { params: { name, page } });
+    const resp = await fetcher.get<GetBakeriesResponse>('/bakeries/search', { params: { name, page } });
     return { bakeries: resp.data.contents, totalCount: resp.data.totalElements, totalPages: resp.data.totalPages };
   }
 }
--- src/apis/bakeryReport/bakeryReportClient.ts
+++ src/apis/bakeryReport/bakeryReportClient.ts
@@ -10,16 +10,16 @@ import { fetcher } from '@/apis/axios';
 
 export class BakeryReportClient implements BakeryReportApiClient {
   async getItem({ reportId }: GetBakeryReportPayload) {
-    const resp = await fetcher.get<BakeryReportsItemEntity>(`/bakery/report/${reportId}`);
+    const resp = await fetcher.get<BakeryReportsItemEntity>(`/bakery-add-reports/${reportId}`);
     return resp.data;
   }
 
   async updateItemStatus({ reportId, status }: UpdateReportStatusPayload) {
-    await fetcher.patch(`/bakery/report/${reportId}`, { status });
+    await fetcher.patch(`/bakery-add-reports/${reportId}`, { status });
   }
 
   async getList({ page }: Omit<GetBakeriesPayload, 'name'>) {
-    const resp = await fetcher.get<GetBakeryReportsResponse>('/bakery/report', { params: { page } });
+    const resp = await fetcher.get<GetBakeryReportsResponse>('/bakery-add-reports', { params: { page } });
     return { bakeryReports: resp.data.contents, totalCount: resp.data.totalElements, totalPages: resp.data.totalPages };
   }
 }
