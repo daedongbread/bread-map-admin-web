e8a6b54b8a67f49a4d841c9229e022620118084d
yjkim <kimyu28jin@gmail.com>
2023-04-07 20:20:34 +0900

    feat: 사진 선택후 폼 반영, 저장 플로우 구현
    - 새로운 이미지 업로드 후 (폼)반영하기 및 저장하기 구현
    - 기존 이미지 (폼)반영하기 및 저장하기 구현

--- src/components/BakeryDetail/Report/ImageEditView/ImageEditView.tsx
+++ src/components/BakeryDetail/Report/ImageEditView/ImageEditView.tsx
@@ -6,7 +6,10 @@ import { Pagination } from '@/components/Shared';
 import { BAKERY_IMG_TAB } from '@/constants';
 import usePagination from '@/hooks/usePagination';
 import useTab from '@/hooks/useTab';
+import { useAppDispatch } from '@/store/hooks';
+import { changeForm } from '@/store/slices/bakery';
 import { Divider } from '@/styles';
+import { urlToFile } from '@/utils';
 import styled from '@emotion/styled';
 
 type Props = {
@@ -18,16 +21,13 @@ export const ImageEditView = ({ bakeryId }: Props) => {
   const { tabs: imgTabs, selectTab: selectImgTab, setTabCount: setImgTabCount } = useTab({ tabData: BAKERY_IMG_TAB });
   const [activeTab, setActiveTab] = useState(imgTabs.find(tab => tab.isActive));
   const { bakeryImagesQuery, uploadImage } = useBakery({ bakeryId });
-  const {
-    data,
-    isLoading: isLoadingSearch,
-    isFetching: isFetchingSearch,
-  } = bakeryImagesQuery({
+  const { data, isLoading, isFetching } = bakeryImagesQuery({
     bakeryId: bakeryId,
     imageType: activeTab!.value as keyof BakeryImageType as BakeryImageType,
     page: currPage,
   });
-  const [changeImage, setChangeImage] = useState<BakeryImageEntity | undefined>();
+  const [selectedImage, setChangeImage] = useState<BakeryImageEntity | undefined>();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (data && data.totalPages) {
@@ -45,31 +45,34 @@ export const ImageEditView = ({ bakeryId }: Props) => {
     setChangeImage(image);
   };
 
-  const onCreateImage = async () => {
-    let imageUrl = changeImage?.image;
-    const imageId = changeImage?.imageId;
-
-    if (imageId! < 0 && imageUrl) {
-      const formData = new FormData();
-      const file = await urlToFile(imageUrl, 'holly.jpg', 'image/jpeg');
-      formData.append('image', file);
-      const result = await uploadImage.mutateAsync({ payload: formData });
-      imageUrl = result.imagePath;
+  const createAndGetImageUrl = async () => {
+    const imageUrl = selectedImage?.image;
+    if (!imageUrl) {
+      return;
     }
-    // TODO 현재 이미지로 반영 작업 (imageUrl)
+
+    const formData = new FormData();
+    const file = await urlToFile(imageUrl, 'holly.jpg');
+    formData.append('image', file);
+    const result = await uploadImage.mutateAsync({ payload: formData });
+    return result.imagePath;
   };
-  const urlToFile = async (url: string, filename: string, mimeType: string) => {
-    const response = await fetch(url, { mode: 'no-cors' });
-    const buffer = await response.arrayBuffer();
-    const file = new File([buffer], filename, { type: mimeType });
-    return file;
+
+  const fillBakeryFormImage = async ({ isFromUser, imageUrl }: { isFromUser: boolean; imageUrl: string }) => {
+    let url = imageUrl;
+
+    if (isFromUser) {
+      const result = await createAndGetImageUrl();
+      result ? (url = result) : window.alert('이미지 반영을 실패했습니다. 다시 시도해주세요.');
+    }
+    await dispatch(changeForm({ name: 'image', value: url as never }));
   };
 
   return (
     <Container>
-      <ImageDiffUploader changeImage={changeImage} onChangeImage={onChangeImage} onCreateImage={onCreateImage} />
+      <ImageDiffUploader selectedImage={selectedImage} onChangeImage={onChangeImage} fillBakeryFormImage={fillBakeryFormImage} />
       <Divider />
-      <Gallery imgTabs={imgTabs} onSelectTab={selectImgTab} images={data?.images ?? []} selectedImage={changeImage} onChangeImage={onChangeImage} />
+      <Gallery imgTabs={imgTabs} onSelectTab={selectImgTab} images={data?.images ?? []} selectedImage={selectedImage} onChangeImage={onChangeImage} />
       <Pagination
         pages={pages}
         currPage={currPage}
