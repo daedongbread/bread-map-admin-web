a0e0d180c0dfd93e49a89979f6b637bdb902a43c
yjkim <kimyu28jin@gmail.com>
2023-04-29 22:06:47 +0900

    fix: 빵집 생성 validation 추가

--- src/utils/bakery.tsx
+++ src/utils/bakery.tsx
@@ -1,20 +1,25 @@
 import { BakeryForm } from '@/store/slices/bakery';
 
 export const validateForm = (form: BakeryForm) => {
-  let validate = true;
+  let isValid = true;
 
   if (!form.name || !form.address || !form.latitude || !form.longitude) {
     window.alert('빵집 이름 혹은 주소(위도, 경도)를 입력해주세요.');
-    validate = false;
+    isValid = false;
+  }
+
+  if (form.address.length < 3 || form.address.length > 100) {
+    window.alert('주소는 3자 이상, 100자 이하로 입력해주세요.');
+    isValid = false;
   }
 
   if (form.productList && form.productList.length > 0) {
     form.productList.forEach(item => {
       if (!item.productName || !item.productType || !item.price) {
         window.confirm('메뉴명, 가격을 입력해주세요.');
-        validate = false;
+        isValid = false;
       }
     });
   }
-  return validate;
+  return isValid;
 };
