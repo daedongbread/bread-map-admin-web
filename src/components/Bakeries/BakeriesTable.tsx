f527702adeb5bb40dbf3fc40144fdc4936277a30
yjkim <kimyu28jin@gmail.com>
2023-05-06 14:46:24 +0900

    feat: 검색결과 없는 경우 UI 작업

--- src/components/Bakeries/BakeriesTable.tsx
+++ src/components/Bakeries/BakeriesTable.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Table } from '@/components/Shared';
+import { Error as Empty, Table } from '@/components/Shared';
 import type { TableProps } from '@/components/Shared';
 import { PATH } from '@/constants';
+import styled from '@emotion/styled';
 
 const event = {
   hover: {
@@ -14,5 +15,18 @@ const event = {
 };
 
 export const BakeriesTable = ({ headers, rows }: TableProps) => {
+  if (!rows.length) {
+    return (
+      <EmptyContainer>
+        <Empty errMsg={'검색 결과가 없습니다.'} />
+      </EmptyContainer>
+    );
+  }
   return <Table headers={headers} rows={rows} event={event} />;
 };
+
+const EmptyContainer = styled.div`
+  height: 70rem;
+  display: flex;
+  justify-content: center;
+`;
